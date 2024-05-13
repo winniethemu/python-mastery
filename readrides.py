05af15b952a71b0123a3a7c90e2c7805cb8fe155
Mu Ye <emailyemu@gmail.com>
2024-05-12 23:50:22 -0700

    2.1d

--- readrides.py
+++ readrides.py
@@ -1,7 +1,49 @@
+from collections import namedtuple
 import csv
 
 
-def read_rides_as_tuples(filename):
+class Row:
+    def __init__(self, route, date, daytype, rides):
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+
+class SlottedRow(Row):
+    __slots__ = ['route', 'date', 'daytype', 'rides']
+
+
+NamedTupleRow = namedtuple(
+    'NamedTupleRow', ('route', 'date', 'daytype', 'rides'))
+
+
+def as_tuple(route, date, daytype, rides):
+    return (route, date, daytype, rides)
+
+
+def as_dict(route, date, daytype, rides):
+    return {
+        'route': route,
+        'date': date,
+        'daytype': daytype,
+        'rides': rides,
+    }
+
+
+def as_class(route, date, daytype, rides):
+    return Row(route, date, daytype, rides)
+
+
+def as_named_tuple(route, date, daytype, rides):
+    return NamedTupleRow(route, date, daytype, rides)
+
+
+def as_class_and_slots(route, date, daytype, rides):
+    return SlottedRow(route, date, daytype, rides)
+
+
+def read_rides(filename, helper):
     '''
     Read the bus ride data as a list of tuples
     '''
@@ -11,13 +53,17 @@ def read_rides_as_tuples(filename):
         headings = next(rows)
         for row in rows:
             route, date, daytype, rides = row
-            record = (route, date, daytype, rides)
+            record = helper(route, date, daytype, rides)
             records.append(record)
     return records
 
 
 if __name__ == '__main__':
     import tracemalloc
+
+    helpers = (as_tuple, as_dict, as_class, as_named_tuple, as_class_and_slots)
     tracemalloc.start()
-    rows = read_rides_as_tuples('Data/ctabus.csv')
-    print('Memory Use: Current %d, Peak %d' % tracemalloc.get_traced_memory())
+    for helper in helpers:
+        rows = read_rides('Data/ctabus.csv', helper)
+        print('Memory Use: Current %d, Peak %d' %
+              tracemalloc.get_traced_memory())
