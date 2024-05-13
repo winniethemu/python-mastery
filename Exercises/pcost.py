941f4f8f4cd464ae6f0853f211a2ccfb2f07714e
Mu Ye <emailyemu@gmail.com>
2024-05-12 19:22:02 -0700

    1.4b

--- Exercises/pcost.py
+++ Exercises/pcost.py
@@ -2,9 +2,12 @@ def portfolio_cost(filename: str) -> float:
     total = 0.0
     with open(filename) as f:
         for line in f:
-            name, shares, price = line.split()
-            total += int(shares) * float(price)
+            try:
+                name, shares, price = line.split()
+                total += int(shares) * float(price)
+            except ValueError as e:
+                print(f'Couldn\'t parse: {line}Reason: {e}')
     return total
 
 
-print(portfolio_cost('../Data/portfolio.dat'))
+print(portfolio_cost('../Data/portfolio3.dat'))
