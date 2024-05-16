61f5d1c2c6b643ab8661afc564a24982b86ecbb6
Mu Ye <emailyemu@gmail.com>
2024-05-15 17:49:26 -0700

    2.4b

--- mutint.py
+++ mutint.py
@@ -3,3 +3,12 @@ class MutInt:
 
     def __init__(self, value):
         self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return f'MutInt({self.value!r})'
+
+    def __format__(self, fmt):
+        return format(self.value, fmt)
