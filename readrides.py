1cf5125d1e9e84de0c8facd60fb5911f36b0abfb
Mu Ye <emailyemu@gmail.com>
2024-05-17 00:06:47 -0700

    2.5d

--- readrides.py
+++ readrides.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from collections import abc, namedtuple
 import csv
 
 
@@ -12,6 +12,33 @@ class Row:
 
 class SlottedRow(Row):
     __slots__ = ['route', 'date', 'daytype', 'rides']
+
+
+class RideData(abc.Sequence):
+    def __init__(self):
+        self.routes = []
+        self.dates = []
+        self.daytypes = []
+        self.numrides = []
+
+    def __len__(self):
+        if (len(self.routes) == len(self.dates) == len(self.daytypes) == len(
+                self.numrides)):
+            return len(self.routes)
+        raise ValueError('inconsistent column lengths')
+
+    def __getitem__(self, index):
+        return {
+                'route': self.routes[index],
+                'date': self.dates[index],
+                'daytype': self.daytypes[index],
+                'rides': self.numrides[index]}
+
+    def append(self, d):
+        self.routes.append(d['route'])
+        self.dates.append(d['date'])
+        self.daytypes.append(d['daytype'])
+        self.numrides.append(d['rides'])
 
 
 NamedTupleRow = namedtuple(
@@ -47,7 +74,7 @@ def read_rides(filename, helper):
     '''
     Read the bus ride data as a list of tuples
     '''
-    records = []
+    records = RideData()
     with open(filename) as f:
         rows = csv.reader(f)
         next(rows)
@@ -58,32 +85,10 @@ def read_rides(filename, helper):
     return records
 
 
-def read_rides_as_columns(filename):
-    '''
-    Read the bus ride data into 4 lists, representing columns
-    '''
-    routes = []
-    dates = []
-    daytypes = []
-    numrides = []
-    with open(filename) as f:
-        rows = csv.reader(f)
-        next(rows)
-        for row in rows:
-            routes.append(row[0])
-            dates.append(row[1])
-            daytypes.append(row[2])
-            numrides.append(row[3])
-    return dict(routes=routes, dates=dates, daytypes=daytypes,
-                numrides=numrides)
-
-
 if __name__ == '__main__':
     import tracemalloc
 
-    helpers = (as_tuple, as_dict, as_class, as_named_tuple, as_class_and_slots)
     tracemalloc.start()
-    for helper in helpers:
-        rows = read_rides('Data/ctabus.csv', helper)
-        print('Memory Use: Current %d, Peak %d' %
-              tracemalloc.get_traced_memory())
+    rows = read_rides('Data/ctabus.csv', as_dict)
+    print('Memory Use: Current %d, Peak %d' %
+          tracemalloc.get_traced_memory())
