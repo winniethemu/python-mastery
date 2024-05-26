4219fbd4b4b9f1da63cbe458ff5e8e7f81de3950
Mu Ye <emailyemu@gmail.com>
2024-05-26 11:04:47 -0700

    3.1c

--- stock.py
+++ stock.py
@@ -6,6 +6,19 @@ def read_portfolio(filename):
     return [Stock(s['name'], s['shares'], s['price']) for s in portfolio]
 
 
+def print_portfolio(portfolio, show_header=True):
+    if show_header:
+        print(f'{"name":>10s} {"shares":>10s} {"price":>10s}')
+        divider = ''
+        for i in range(3):
+            divider += '-' * 10
+            if i < 3:
+                divider += ' '
+        print(divider)
+    for stock in portfolio:
+        print(f'{stock.name:>10s} {stock.shares:>10d} {stock.price:>10.2f}')
+
+
 class Stock:
     def __init__(self, name, shares, price):
         self.name = name
