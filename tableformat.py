b68e8b049a1993658561791ee0e29d49a331324b
Mu Ye <emailyemu@gmail.com>
2024-05-31 22:49:32 -0700

    3.5a

--- tableformat.py
+++ tableformat.py
@@ -1,12 +1,25 @@
-def print_table(records, attributes):
-    column_width = 10
-    header = ' '.join([f'%{column_width}s' %
-                      attribute for attribute in attributes])
-    divider = ('-' * column_width + ' ') * len(attributes)
-    print(header)
-    print(divider)
-
+def print_table(records, attributes, formatter):
+    formatter.headings(attributes)
     for record in records:
-        row = ' '.join([f'%{column_width}s' % getattr(
-            record, attribute) for attribute in attributes])
-        print(row)
+        row_data = [getattr(record, attribute) for attribute in attributes]
+        formatter.row(row_data)
+
+    # column_width = 10
+    # header = ' '.join([f'%{column_width}s' %
+    #                   attribute for attribute in attributes])
+    # divider = ('-' * column_width + ' ') * len(attributes)
+    # print(header)
+    # print(divider)
+
+    # for record in records:
+    #     row = ' '.join([f'%{column_width}s' % getattr(
+    #         record, attribute) for attribute in attributes])
+    #     print(row)
+
+
+class TableFormatter:
+    def headings(self, headers):
+        raise NotImplementedError()
+
+    def row(self, row_data):
+        raise NotImplementedError()
